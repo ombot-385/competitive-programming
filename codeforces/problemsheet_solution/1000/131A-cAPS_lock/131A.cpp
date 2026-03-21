2db205baf17d823452ad67bf5597ad651671b818
ombot-385 <omkar.kumar.dev@gmail.com>
2026-03-21 16:00:46 +0530

    Add files via upload

---
+++ codeforces/problemsheet_solution/1000/131A-cAPS_lock/131A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    
+    bool allcaps= true;
+    for(int i=1;i<s.length();i++){
+        if(islower(s[i])){
+            allcaps=false;
+            break;
+        }
+    }
+    
+    if(allcaps){
+        for(int i=0;i<s.length();i++){
+            if(islower(s[i])){
+                s[i]=toupper(s[i]);
+            }
+            else{
+                s[i]=tolower(s[i]);
+            }
+        }
+    }
+    cout<<s;
+    return 0;
+}
